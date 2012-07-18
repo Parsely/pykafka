07bce906147d8cf173f78b55aa2954b01e14d759
Ted Kaemming <ted@kaemming.com>
2012-07-18 10:15:18 -0700

    add kazoo to requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@ try:
 except ImportError:
     pass
 
+install_requires = [
+    'zc-zookeeper-static',
+    'kazoo',
+]
+
+dependency_links = [
+    'https://github.com/python-zk/kazoo/zipball/master#egg=kazoo',
+]
+
 tests_require = ['mock', 'nose', 'unittest2']
 
 setup_requires = []
@@ -17,8 +26,10 @@ if 'nosetests' in sys.argv[1:]:
 setup(
     name='samsa',
     packages=('samsa',),
+    install_requires=install_requires,
     tests_require=tests_require,
     setup_requires=setup_requires,
+    dependency_links=dependency_links,
     zip_safe=False,
     test_suite='nose.collector',
 )
