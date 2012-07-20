4d80a387ee173e616e2043a414a3c8e3fc78b56a
Ted Kaemming <ted@kaemming.com>
2012-07-20 11:42:19 -0700

    add integration test stub

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 doc:
 	cd doc/ && make html
 
+unit:
+	python setup.py nosetests --attr=!integration
+
+integration:
+	python setup.py nosetests --attr=integration
+
 test:
 	python setup.py test
 
-.PHONY: doc test
+.PHONY: doc unit integration test
--- tests/samsa/client/tests.py
+++ tests/samsa/client/tests.py
@@ -1,10 +1,27 @@
 import mock
 import unittest2
-
-from samsa.client import TIME_EARLIEST, TIME_LATEST, Client
+from nose.plugins.attrib import attr
 
 
 class ClientTestCase(unittest2.TestCase):
+    def test_produce(self):
+        raise NotImplementedError
+
+    def test_multiproduce(self):
+        raise NotImplementedError
+
+    def test_fetch(self):
+        raise NotImplementedError
+
+    def test_multifetch(self):
+        raise NotImplementedError
+
+    def test_offsets(self):
+        raise NotImplementedError
+
+
+@attr('integration')
+class ClientIntegrationTestCase(unittest2.TestCase):
     def test_produce(self):
         raise NotImplementedError
 
