ebed91cfe16edd748289863e4d6955ed308220a8
Ted Kaemming <ted@kaemming.com>
2012-07-24 16:35:26 -0700

    integration tests

--- samsa/test/integration.py
+++ samsa/test/integration.py
@@ -1,8 +1,9 @@
-import contextlib
+import logging
 import os
 import subprocess
 import random
 import tempfile
+import threading
 import time
 
 import unittest2
@@ -10,53 +11,162 @@ from kazoo.testing import KazooTestHarness
 from nose.plugins.attrib import attr
 
 
+logger = logging.getLogger(__name__)
+
+
+def merge(*dicts):
+    return reduce(lambda x, y: dict(x, **y), dicts, {})
+
+
+class TimeoutError(Exception):
+    pass
+
+
+def polling_timeout(predicate, duration, interval=0.1, error='timeout exceeded'):
+    for _ in xrange(0, int(duration / interval)):
+        if predicate():
+            return
+        time.sleep(interval)
+    else:
+        raise TimeoutError(error)
+
+
 @attr('integration')
-class IntegrationTestCase(unittest2.TestCase, KazooTestHarness):
+class KafkaIntegrationTestCase(unittest2.TestCase, KazooTestHarness):
+    KAFKA_BROKER_CONFIGURATION = {
+        'brokerid': 0,
+        'port': 9092,
+        'num.partitions': 1,
+        'enable.zookeeper': 'true',
+        'log.flush.interval': 1,  # commit messages as soon as possible
+    }
+
+    KAFKA_OPTIONS = {
+        'heap_max': '512M',
+    }
+
+    KAFKA_LOGGING_CONFIGURATION = {
+        'log4j.rootLogger': 'INFO, stderr',
+        'log4j.appender.stderr': 'org.apache.log4j.ConsoleAppender',
+        'log4j.appender.stderr.target': 'System.err',
+        'log4j.appender.stderr.layout': 'org.apache.log4j.PatternLayout',
+        'log4j.appender.stderr.layout.ConversionPattern': '%c: %p: %m%n',
+        'log4j.logger.kafka': 'INFO',
+    }
+
     def setUp(self):
         self.setup_zookeeper()
-        self.setup_kafka()
+        self.setup_kafka_broker()
 
     def tearDown(self):
-        self.teardown_kafka()
+        self.teardown_kafka_broker()
         self.teardown_zookeeper()
 
-    def setup_kafka(self):
-        self._kafka_directory = tempfile.mkdtemp()
-        server = random.choice(list(self.cluster))
-        configuration = {
-            'brokerid': 0,
-            'port': 9092,
-            'log.dir': self._kafka_directory,
-            'num.partitions': 1,
-            'enable.zookeeper': 'true',
-            'zk.connect': server.address,
-        }
-
-        self._kafka_configuration_file = tempfile.NamedTemporaryFile(delete=False)
-        with contextlib.closing(self._kafka_configuration_file):
-            for key, value in configuration.iteritems():
-                self._kafka_configuration_file.write('%s=%s\n' % (key, value))
-
+    def __run_class(self, cls, *args, **kwargs):
         executable = os.path.join(os.path.dirname(__file__), 'kafka-run-class.sh')
+        args = (executable, cls) + args
 
-        null = open('/dev/null')
-        args = [executable, 'kafka.Kafka', self._kafka_configuration_file.name]
+        kwargs['env'] = kwargs.get('env', os.environ).copy()
+        kwargs['env'].update({
+            'KAFKA_OPTS': '-Xmx%(heap_max)s -server -Dlog4j.configuration=file:%(logging_config)s' % \
+                merge(self.KAFKA_OPTIONS, {
+                    'logging_config': self.__kafka_logging_configuration_file.name,
+                })
+        })
 
-        # TODO: Allow redirection to a logging handler instead of squelching it.
-        self._kafka_broker = subprocess.Popen(args, stdout=null, stderr=null)
-        time.sleep(1)
+        process = subprocess.Popen(args, stderr=subprocess.PIPE, **kwargs)
 
-    def teardown_kafka(self):
-        self._kafka_broker.terminate()
+        def convert_log_output(process, namespace):
+            while process.returncode is None:
+                line = process.stderr.readline().strip()
+                if not line:
+                    continue
 
-        timeout = 5
+                try:
+                    name, level, message = (bit.strip() for bit in line.split(':', 2))
+                    logging.getLogger('%s:%s' % (namespace, name)).log(getattr(logging, level.upper()), message)
+                except ValueError:
+                    logging.getLogger('%s:unfiltered' % namespace).warning(line)
+
+        logthread = threading.Thread(target=convert_log_output, args=(process, cls))
+        logthread.daemon = True  # shouldn't be necessary, but just in case
+        logthread.start()
+
+        return process
+
+    def __write_property_file(self, properties):
+        file = tempfile.NamedTemporaryFile(delete=False)
+        for item in properties.iteritems():
+            file.write('%s=%s\n' % item)
+        file.close()
+        return file
+
+    def setup_kafka_broker(self):
+        self.__kafka_directory = tempfile.mkdtemp()
+
+        # Logging Configuratin
+        self.__kafka_logging_configuration_file = \
+            self.__write_property_file(self.KAFKA_LOGGING_CONFIGURATION)
+
+        # Broker Configuration
+        broker_configuration = merge(self.KAFKA_BROKER_CONFIGURATION, {
+            'log.dir': self.__kafka_directory,
+            'zk.connect': self.hosts,
+        })
+        self.__kafka_broker_configuration_file = \
+            self.__write_property_file(broker_configuration)
+
+        ready = threading.Event()
+        path = '/brokers/ids/%(brokerid)s' % broker_configuration
+        if self.client.exists(path, watch=lambda *a, **k: ready.set()) is not None:
+            raise AssertionError('Kafka broker is already running!')
+
+        logger.info('Starting Kafka broker...')
+        self.kafka_broker = self.__run_class('kafka.Kafka', self.__kafka_broker_configuration_file.name)
+
+        timeout = 3
         for _ in xrange(0, timeout):
-            self._kafka_broker.poll()
-            if self._kafka_broker.returncode is None:
-                time.sleep(1)
-            else:
+            if ready.is_set():
                 break
+            ready.wait(timeout=1)
         else:
-            self._kafka_broker.kill()
+            raise TimeoutError('Kafka broker did not start within %s seconds' % timeout)
 
-        # TODO: Clean up temporary directory and configuration files.
+    def teardown_kafka_broker(self):
+        self.kafka_broker.terminate()
+
+        def broker_exited():
+            self.kafka_broker.poll()
+            return self.kafka_broker.returncode is not None
+
+        logger.debug('Shutting down Kafka broker...')
+        try:
+            polling_timeout(broker_exited, 3)
+        except TimeoutError:
+            logger.info('Broker did not exit cleanly within timeout, sending SIGKILL...')
+            self.kafka_broker.kill()
+        finally:
+            # TODO: Clean up temporary directory and configuration files.
+            pass
+
+    def consumer(self, topic, group='test-consumer-group'):
+        # TODO: Clean up after self.
+        configuration_file = self.__write_property_file({
+            'zk.connect': self.hosts,
+            'groupid': group,
+        })
+        return self.__run_class('kafka.tools.ConsumerShell',
+            '--topic', topic, '--props', configuration_file.name,
+            stdout=subprocess.PIPE)
+
+    def producer(self, topic, **kwargs):
+        # TODO: Clean up after self.
+        configuration_file = self.__write_property_file({
+            'zk.connect': self.hosts,
+            'producer.type': 'sync',
+            'compression.codec': 0,
+            'serializer.class': 'kafka.serializer.StringEncoder',
+        })
+        return self.__run_class('kafka.tools.ProducerShell',
+            '--topic', topic, '--props', configuration_file.name,
+            **kwargs)
--- tests/samsa/client/tests.py
+++ tests/samsa/client/tests.py
@@ -1,39 +1,125 @@
+import itertools
 import mock
+import subprocess
+import time
 import unittest2
 
-from samsa.client import Client
-from samsa.test.integration import IntegrationTestCase
+from samsa.client import Client, OFFSET_EARLIEST, OFFSET_LATEST
+from samsa.test.integration import KafkaIntegrationTestCase
 
 
-class ClientTestCase(unittest2.TestCase):
+# class ClientTestCase(unittest2.TestCase):
+#     def test_produce(self):
+#         raise NotImplementedError
+#
+#     def test_multiproduce(self):
+#         raise NotImplementedError
+#
+#     def test_fetch(self):
+#         raise NotImplementedError
+#
+#     def test_multifetch(self):
+#         raise NotImplementedError
+#
+#     def test_offsets(self):
+#         raise NotImplementedError
+
+
+def filter_messages(stream):
+    # TODO: this can deadlock, needs a timeout or something
+    prefix = 'consumed: '
+    while True:
+        line = stream.readline().strip()
+        if line.startswith(prefix):
+            message = line[len(prefix):]
+            yield message
+
+
+class ClientIntegrationTestCase(KafkaIntegrationTestCase):
+    def setUp(self):
+        super(ClientIntegrationTestCase, self).setUp()
+        self.kafka = Client(host='localhost')
+
     def test_produce(self):
-        raise NotImplementedError
+        topic = 'topic'
+        consumer = self.consumer(topic)
+        message = 'hello world'
+        self.kafka.produce(topic, 0, (message,))
+
+        consumed = next(filter_messages(consumer.stdout))
+        self.assertEqual(consumed, message)
+
+        consumer.terminate()
+        consumer.wait()
 
     def test_multiproduce(self):
-        raise NotImplementedError
+        topics = ('topic-a', 'topic-b')
+
+        def message_for_topic(topic):
+            return 'hello to topic %s' % topic
+
+        consumers = {}
+        for topic in topics:
+            consumers[topic] = self.consumer(topic)
+
+        batch = []
+        for topic in topics:
+            batch.append((topic, 0, (message_for_topic(topic),)))
+
+        self.kafka.multiproduce(batch)
+
+        for topic, consumer in consumers.items():
+            consumed = next(filter_messages(consumer.stdout))
+            self.assertEqual(consumed, message_for_topic(topic))
+            consumer.terminate()
+            consumer.wait()
 
     def test_fetch(self):
-        raise NotImplementedError
+        topic = 'topic'
+        message = 'hello world'
+        size = 1024 * 300
+
+        producer = self.producer(topic, stdin=subprocess.PIPE)
+        producer.stdin.write('%s\n' % message)
+        producer.stdin.flush()
+        time.sleep(1)  # TODO: Not this
+        producer.terminate()
+        producer.wait()
+
+        messages = list(self.kafka.fetch(topic, 0, 0, size))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0][0], 0)
+        self.assertEqual(messages[0][1], message)
 
     def test_multifetch(self):
-        raise NotImplementedError
+        topics = ('topic-a', 'topic-b')
+        size = 1024 * 300
+
+        def message_for_topic(topic):
+            return 'hello from topic %s' % topic
+
+        for topic in topics:
+            producer = self.producer(topic, stdin=subprocess.PIPE)
+            producer.stdin.write('%s\n' % message_for_topic(topic))
+            producer.stdin.flush()
+            time.sleep(1)  # TODO: Not this
+            producer.terminate()
+            producer.wait()
+
+        batches = [(topic, 0, 0, size) for topic in topics]
+        responses = self.kafka.multifetch(batches)
+
+        num_responses = 0
+        for topic, response in zip(topics, responses):
+            messages = list(response)
+            self.assertEqual(len(messages), 1)
+            self.assertEqual(messages[0][0], 0)
+            self.assertEqual(messages[0][1], message_for_topic(topic))
+            num_responses += 1
+
+        self.assertEqual(len(batches), num_responses)
 
     def test_offsets(self):
-        raise NotImplementedError
-
-
-class ClientIntegrationTestCase(IntegrationTestCase):
-    def test_produce(self):
-        raise NotImplementedError
-
-    def test_multiproduce(self):
-        raise NotImplementedError
-
-    def test_fetch(self):
-        raise NotImplementedError
-
-    def test_multifetch(self):
-        raise NotImplementedError
-
-    def test_offsets(self):
-        raise NotImplementedError
+        offsets = self.kafka.offsets('topic', 0, OFFSET_EARLIEST, 1)
+        self.assertEqual(len(offsets), 1)
+        self.assertEqual(offsets[0], 0)
