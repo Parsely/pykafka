40b2529bedd1d097636c493e9ae8371c23a5c114
Keith Bourgoin <keith@parsely.com>
2013-04-24 22:10:07 -0400

    Update copyright notice for files we've edited

--- samsa/config.py
+++ samsa/config.py
@@ -1,5 +1,6 @@
 __license__ = """
 Copyright 2012 DISQUS
+Copyright 2013 Parse.ly, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
