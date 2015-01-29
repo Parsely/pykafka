7560f7289a120bf380b9630b0756a4d357d3139b
Keith Bourgoin <keith@parsely.com>
2015-01-29 14:26:51 -0500

    Forgot to change this name.

--- kafka/pykafka/__init__.py
+++ kafka/pykafka/__init__.py
@@ -1,5 +1,5 @@
 from broker import Broker
-from consumer import Consumer
+from consumer import SimpleConsumer
 from cluster import Cluster
 from partition import Partition
 from producer import Producer
