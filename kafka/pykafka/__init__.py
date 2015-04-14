8c1b53a156e7f54e3310d58392db8bc125af9c86
Emmett Butler <emmett.butler321@gmail.com>
2015-04-14 15:46:59 -0700

    fix import

--- kafka/pykafka/__init__.py
+++ kafka/pykafka/__init__.py
@@ -1,5 +1,5 @@
 from broker import Broker
-from consumer import SimpleConsumer
+from simpleconsumer import SimpleConsumer
 from cluster import Cluster
 from partition import Partition
 from producer import Producer
