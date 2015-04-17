6acfa329ea7b3cd05801ba2dffecf7f4a9c62f2c
Emmett Butler <emmett.butler321@gmail.com>
2015-04-17 11:37:42 -0700

    have partition responses share a common subclass

--- pykafka/pykafka/protocol.py
+++ pykafka/pykafka/protocol.py
@@ -101,6 +101,11 @@ class Response(object):
                 clsname, response))
 
 
+class PartitionResponse(object):
+    def __init__(self, error):
+        self.error = error
+
+
 class Message(common.Message, Serializable):
     """Representation of a Kafka Message
 
@@ -605,7 +610,7 @@ class FetchRequest(Request):
         return output
 
 
-class FetchPartitionResponse(object):
+class FetchPartitionResponse(PartitionResponse):
     """Partition information that's part of a FetchResponse"""
     def __init__(self, max_offset, messages, error):
         """Create a new FetchPartitionResponse
@@ -613,9 +618,9 @@ class FetchPartitionResponse(object):
         :param max_offset: The offset at the end of this partition
         :param messages: Messages in the response
         """
+        super(FetchPartitionResponse, self).__init__(error)
         self.max_offset = max_offset
         self.messages = messages
-        self.error = error
 
 
 class FetchResponse(Response):
@@ -739,6 +744,18 @@ class OffsetRequest(Request):
                                  pnum, offsets_before, max_offsets)
                 offset += 16
         return output
+
+
+class OffsetPartitionResponse(PartitionResponse):
+    """Partition information that's part of an OffsetResponse"""
+    def __init__(self, offset, error):
+        """Create a new OffsetPartitionResponse
+
+        :param offset: The requested offset
+        :param error: The error code
+        """
+        super(OffsetPartitionResponse, self).__init__(error)
+        self.offset = offset
 
 
 class OffsetResponse(Response):
@@ -937,6 +954,16 @@ class OffsetCommitRequest(Request):
         return output
 
 
+class OffsetCommitPartitionResponse(PartitionResponse):
+    """Partition information that's part of an OffsetCommitResponse"""
+    def __init__(self, error):
+        """Create a new OffsetCommitPartitionResponse
+
+        :param error: The error code for this partition
+        """
+        super(OffsetCommitPartitionResponse, self).__init__(error)
+
+
 class OffsetCommitResponse(Response):
     """An offset commit response
 
@@ -1042,7 +1069,7 @@ class OffsetFetchRequest(Request):
         return output
 
 
-class OffsetFetchPartitionResponse(object):
+class OffsetFetchPartitionResponse(PartitionResponse):
     """Partition information that's part of an OffsetFetchResponse"""
     def __init__(self, offset, metadata, error):
         """Create a new OffsetFetchPartitionResponse
@@ -1050,9 +1077,9 @@ class OffsetFetchPartitionResponse(object):
         :param offset:
         :param metadata: arbitrary metadata that should be committed with this offset commit
         """
+        super(OffsetFetchPartitionResponse, self).__init__(error)
         self.offset = offset
         self.metadata = metadata
-        self.error = error
 
 
 class OffsetFetchResponse(Response):
