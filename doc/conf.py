3873454ac7d5d4611bda5604767a3bed696ca1d4
Emmett Butler <emmett.butler321@gmail.com>
2015-04-23 14:44:00 -0700

    parent-dir import hack for conf

--- doc/conf.py
+++ doc/conf.py
@@ -15,6 +15,12 @@ limitations under the License.
 """
 
 # -*- coding: utf-8 -*-
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
 from version import version
 
 project = u'pykafka'
