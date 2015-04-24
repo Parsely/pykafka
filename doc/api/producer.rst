de092476437dcd1aed6fd90fd1afdfd2c973bfdc
Emmett Butler <emmett.butler321@gmail.com>
2015-04-24 11:55:01 -0700

    producer docs

---
+++ doc/api/producer.rst
@@ -0,0 +1,5 @@
+pykafka.producer
+================
+
+.. automodule:: pykafka.producer
+   :members:
--- pykafka/producer.py
+++ pykafka/producer.py
@@ -1,8 +1,26 @@
 from __future__ import division
+"""
+Author: Keith Bourgoin
+"""
+__license__ = """
+Copyright 2015 Parse.ly, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import itertools
-import time
 import logging
+import time
 from collections import defaultdict
 
 import base
@@ -20,7 +38,6 @@ logger = logging.getLogger(__name__)
 
 
 class AsyncProducer(base.BaseAsyncProducer):
-
     def __init__(self,
                  topic,
                  partitioner=None,
@@ -33,24 +50,11 @@ class AsyncProducer(base.BaseAsyncProducer):
                  batch_size=200,
                  batch_time_ms=5000,
                  max_pending_messages=10000):
-        """Create an AsyncProducer for a topic.
-
-        :param topic: The topic to produce messages for.
-        :type topic: :class:`kafka.pykafka.topic.Topic`
-        :para compression: Compression to use for messages.
-        :type compression:
-        :param max_retries: Number of times to retry sending messages.
-        :param retry_backoff_ms: Interval to wait between retries
-        :param topic_refresh_interval_ms: Time between queries to refresh
-            metadata about the topic. The Producer will also refresh this data
-            when the cluster changes (e.g. partitions missing, etc), but this
-            is the interval for how often it actively polls for changes.
-        """
-        pass
+        """Create an AsyncProducer for a topic."""
+        raise NotImplementedError("AsyncProducer is unimplemented")
 
 
 class Producer(base.BaseProducer):
-
     def __init__(self,
                  cluster,
                  topic,
@@ -62,6 +66,41 @@ class Producer(base.BaseProducer):
                  required_acks=1,
                  ack_timeout_ms=10000,
                  batch_size=200):
+        """Instantiate a new Producer.
+
+        This class implements the synchronous producer logic found in the
+        JVM driver.
+
+        :param cluster: The cluster to which to connect
+        :type cluster: :class:`pykafka.cluster.Cluster`
+        :param topic: The topic to which to produce messages
+        :type topic: :class:`pykafka.topic.Topic`
+        :param partitioner: The partitioner to use during message production
+        :type partitioner: :class:`pykafka.partitioners.BasePartitioner`
+        :param compression: The type of compression to use.
+        :type compression: :class:`pykafka.common.CompressionType`
+        :param max_retries: How many times to attempt to produce messages
+            before raising an error.
+        :type max_retries: int
+        :param retry_backoff_ms: The amount of time (in milliseconds) to
+            back off during produce request retries.
+        :type retry_backoff_ms: int
+        :param topic_refresh_interval_ms: Time (in milliseconds) between queries
+            to refresh metadata about the topic. The Producer will also refresh
+            this data when the cluster changes (e.g. partitions missing, etc),
+            but this is the interval for how often it actively polls for
+            changes.
+        :type topic_refresh_interval_ms: int
+        :param required_acks: How many other brokers must have committed the
+            data to their log and acknowledged this to the leader before a
+            request is considered complete?
+        :type required_acks: int
+        :param ack_timeout_ms: Amount of time (in milliseconds) to wait for
+            acknowledgment of a produce request.
+        :type ack_timeout_ms: int
+        :param batch_size: Size (in bytes) of batches to send to brokers.
+        :type batch_size: int
+        """
         # See BaseProduce.__init__.__doc__ for docstring
         self._cluster = cluster
         self._topic = topic
@@ -69,13 +108,20 @@ class Producer(base.BaseProducer):
         self._compression = compression
         self._max_retries = max_retries
         self._retry_backoff_ms = retry_backoff_ms
-        self._topic_refresh_interval_ms = topic_refresh_interval_ms
         self._required_acks = required_acks
         self._ack_timeout_ms = ack_timeout_ms
         self._batch_size = batch_size
 
     def _send_request(self, broker, req, attempt):
-        """Send the request to the broker and handle the response."""
+        """Send the produce request to the broker and handle the response.
+
+        :param broker: The broker to which to send the request
+        :type broker: :class:`pykafka.broker.Broker`
+        :param req: The produce request to send
+        :type req: :class:`pykafka.protocol.ProduceRequest`
+        :param attempt: The current attempt count. Used for retry logic
+        :type attempt: int
+        """
 
         def _get_partition_msgs(partition_id, req):
             """Get all the messages for the partitions from the request."""
@@ -121,7 +167,7 @@ class Producer(base.BaseProducer):
                     to_retry.extend(_get_partition_msgs(partition, req))
         except SocketDisconnectedError:
             logger.warning('Broker %s:%s disconnected. Retrying.',
-                            broker.host, broker.port)
+                           broker.host, broker.port)
             self._cluster.update()
             to_retry = [
                 ((message.partition_key, message.value), p_id)
@@ -185,6 +231,11 @@ class Producer(base.BaseProducer):
             self._send_request(leader, req, attempt)
 
     def produce(self, messages):
+        """Produce a set of messages.
+
+        :param messages: The messages to produce
+        :type messages: Iterable of :class:`pykafka.common.Message`
+        """
         # Do partition distribution here. We need to be able to retry producing
         # only *some* messages when a leader changes. Therefore, we don't want
         # a random partition distribution changing that on the retry.
