b082b827ae30744439795eb893c6b48060cfd4fc
Emmett Butler <emmett.butler321@gmail.com>
2015-04-24 15:58:13 -0700

    include base class in __all__

--- pykafka/topic.py
+++ pykafka/topic.py
@@ -16,7 +16,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-__all__ = ["Topic"]
+__all__ = ["Topic", "base.BaseTopic"]
 import logging
 import weakref
 from collections import defaultdict
