f4e1b8e084b68c35010adb05d7a74f052f9adc95
Emmett Butler <emmett.butler321@gmail.com>
2015-06-08 11:02:13 -0700

    increment version

--- pykafka/__init__.py
+++ pykafka/__init__.py
@@ -7,7 +7,7 @@ from topic import Topic
 from client import KafkaClient
 from balancedconsumer import BalancedConsumer
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 
 __all__ = ["Broker", "SimpleConsumer", "Cluster", "Partition", "Producer",
