bc4c93de45205f15e438dfb3b4360f02aa2ed1a5
Yung-Chin Oei <yungchin@yungchin.nl>
2015-06-12 14:17:28 +0100

    tests/rdkafka: fix offset tests
    These two would sometimes fail, because they assumed (incorrectly) that
    reading 100-odd messages would ensure we'd read from all partitions.
    This highlights a difference between SimpleConsumer and the rdkafka
    consumer: the former will cycle over its partitions message-for-message,
    the latter will take batches off each partition.
    
    Signed-off-by: Yung-Chin Oei <yungchin@yungchin.nl>

--- tests/pykafka/rdkafka/test_simple_consumer.py
+++ tests/pykafka/rdkafka/test_simple_consumer.py
@@ -21,14 +21,14 @@ class TestRdKafkaSimpleConsumer(test_simpleconsumer.TestSimpleConsumer):
         consumer = self._get_simple_consumer(
             consumer_group='test_offset_commit_agrees')
         latest_offs = _latest_partition_offsets_by_reading(consumer, 100)
-
         consumer.commit_offsets()
-        res = consumer.fetch_offsets()
-        consumer.stop()
 
-        # Discard any empty partitions (which thus won't be in latest_offs):
-        retrieved_offs = {r[0]: r[1].offset for r in res if r[1].offset != -1}
+        # We can only compare partitions we've consumed from, so filter:
+        retrieved_offs = {r[0]: r[1].offset for r in consumer.fetch_offsets()
+                          if r[0] in latest_offs}
+
         self.assertEquals(retrieved_offs, latest_offs)
+        consumer.stop()
 
     def test_offset_resume_agrees(self):
         """Check the rdkafka consumer returns messages at specified offset
@@ -46,6 +46,9 @@ class TestRdKafkaSimpleConsumer(test_simpleconsumer.TestSimpleConsumer):
             consumer_group='test_offset_resume_agrees')
         while latest_offs: # check each partition, then tick it off:
             msg = consumer.consume()
+            if msg.partition_id not in latest_offs:
+                # ie we didn't get to this partition previously
+                continue
             expected_offset = latest_offs[msg.partition_id] + 1
             self.assertEquals(msg.offset, expected_offset)
             del latest_offs[msg.partition_id]
