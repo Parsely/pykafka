d2a7e4ca3a6ea85c72fd38b81f638888dd0c2393
Emmett Butler <emmett.butler321@gmail.com>
2016-03-15 16:01:02 -0700

    don't explode worker thread during interpreter shutdown. fixes #399

--- pykafka/handlers.py
+++ pykafka/handlers.py
@@ -188,6 +188,8 @@ class RequestHandler(object):
                     task = shared.requests.get(timeout=1)
                 except Empty:
                     continue
+                except TypeError:  # can happen on interpreter shutdown
+                    break
                 try:
                     shared.connection.request(task.request)
                     if task.future:
