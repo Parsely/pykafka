40bf5e47ba5a7200a435a7202c590fd967bd408d
Emmett Butler <emmett.butler321@gmail.com>
2016-05-13 11:44:15 -0700

    don't pass unsupported kwarg to rdk producer

--- pykafka/topic.py
+++ pykafka/topic.py
@@ -89,6 +89,8 @@ class Topic(object):
         if isinstance(self._cluster.handler, GEventHandler) and use_rdkafka:
             raise ImportError("use_rdkafka cannot be used with gevent")
         Cls = rdkafka.RdKafkaProducer if rdkafka and use_rdkafka else Producer
+        if Cls == rdkafka.RdKafkaProducer:
+            kwargs.pop('block_on_queue_full', None)
         return Cls(self._cluster, self, **kwargs)
 
     def get_sync_producer(self, **kwargs):
