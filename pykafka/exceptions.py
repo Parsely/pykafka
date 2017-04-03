c73f060abbf46f81ccc42798e5dc7e55bb3c65a2
Emmett Butler <emmett.butler321@gmail.com>
2017-04-03 08:52:27 -0700

    support error code 29. related to #674

--- pykafka/exceptions.py
+++ pykafka/exceptions.py
@@ -208,6 +208,13 @@ class RebalanceInProgress(ProtocolClientError):
     ERROR_CODE = 27
 
 
+class TopicAuthorizationFailed(ProtocolClientError):
+    """Returned by the broker when the client is not authorized to access the requested
+        topic.
+    """
+    ERROR_CODE = 29
+
+
 class GroupAuthorizationFailed(ProtocolClientError):
     """Returned by the broker when the client is not authorized to access a particular
     groupId.
@@ -235,6 +242,7 @@ ERROR_CODES = dict(
                 UnknownMemberId,
                 InvalidSessionTimeout,
                 RebalanceInProgress,
+                TopicAuthorizationFailed,
                 GroupAuthorizationFailed)
 )
 
