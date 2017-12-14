117a9d189935ac8caa92338317251ce93ccbe8c1
Emmett Butler <emmett.butler321@gmail.com>
2017-12-14 11:48:34 -0800

    implement all six versions of MetadataResponse

--- pykafka/protocol.py
+++ pykafka/protocol.py
@@ -523,6 +523,9 @@ BrokerMetadata = namedtuple('BrokerMetadata', ['id', 'host', 'port'])
 TopicMetadata = namedtuple('TopicMetadata', ['name', 'partitions', 'err'])
 PartitionMetadata = namedtuple('PartitionMetadata',
                                ['id', 'leader', 'replicas', 'isr', 'err'])
+PartitionMetadataV5 = namedtuple('PartitionMetadataV5',
+                                 ['id', 'leader', 'replicas', 'isr', 'offline_replicas',
+                                  'err'])
 
 
 class MetadataResponse(Response):
@@ -530,20 +533,23 @@ class MetadataResponse(Response):
 
     Specification::
 
-        MetadataResponse => [Broker][TopicMetadata]
-          Broker => NodeId Host Port
-          NodeId => int32
-          Host => string
-          Port => int32
-          TopicMetadata => TopicErrorCode TopicName [PartitionMetadata]
-          TopicErrorCode => int16
-          PartitionMetadata => PartitionErrorCode PartitionId Leader Replicas Isr
-          PartitionErrorCode => int16
-          PartitionId => int32
-          Leader => int32
-          Replicas => [int32]
-          Isr => [int32]
+    Metadata Response (Version: 0) => [brokers] [topic_metadata]
+        brokers => node_id host port
+            node_id => INT32
+            host => STRING
+            port => INT32
+        topic_metadata => error_code topic [partition_metadata]
+            error_code => INT16
+            topic => STRING
+            partition_metadata => error_code partition leader [replicas] [isr]
+                error_code => INT16
+                partition => INT32
+                leader => INT32
+                replicas => INT32
+                isr => INT32
     """
+    API_KEY = 3
+
     def __init__(self, buff):
         """Deserialize into a new Response
 
@@ -553,38 +559,199 @@ class MetadataResponse(Response):
         fmt = '[iSi] [hS [hii [i] [i] ] ]'
         response = struct_helpers.unpack_from(fmt, buff, 0)
         broker_info, topics = response
+        self._populate(broker_info, topics)
+
+    def _populate(self,
+                  broker_info,
+                  topics,
+                  controller_id=None,
+                  cluster_id=None,
+                  throttle_time_ms=0):
+        self.throttle_time_ms = throttle_time_ms
 
         self.brokers = {}
         for (id_, host, port) in broker_info:
             self.brokers[id_] = BrokerMetadata(id_, host, port)
 
+        self.cluster_id = cluster_id
+        self.controller_id = controller_id
+
         self.topics = {}
         for (err, name, partitions) in topics:
-            part_metas = {}
-            for (p_err, id_, leader, replicas, isr) in partitions:
-                part_metas[id_] = PartitionMetadata(id_, leader, replicas,
-                                                    isr, p_err)
-            self.topics[name] = TopicMetadata(name, part_metas, err)
+            self.topics[name] = TopicMetadata(name,
+                                              self._build_partition_metas(partitions),
+                                              err)
+
+    def _build_partition_metas(self, partitions):
+        part_metas = {}
+        for (p_err, id_, leader, replicas, isr) in partitions:
+            part_metas[id_] = PartitionMetadata(id_, leader, replicas,
+                                                isr, p_err)
+        return part_metas
 
 
-class MetadataResponseV1(Response):
+class MetadataResponseV1(MetadataResponse):
+    """Response from MetadataRequest
+
+    Specification::
+
+    Metadata Response (Version: 1) => [brokers] controller_id [topic_metadata]
+        brokers => node_id host port rack
+            node_id => INT32
+            host => STRING
+            port => INT32
+            rack => NULLABLE_STRING  (new since v0)
+        controller_id => INT32  (new since v0)
+        topic_metadata => error_code topic is_internal [partition_metadata]
+            error_code => INT16
+            topic => STRING
+            is_internal => BOOLEAN  (new since v0)
+            partition_metadata => error_code partition leader [replicas] [isr]
+                error_code => INT16
+                partition => INT32
+                leader => INT32
+                replicas => INT32
+                isr => INT32
+    """
+    def __init__(self, buff):
+        fmt = '[iSiS] i [hSb [hii [i] [i] ] ]'
+        response = struct_helpers.unpack_from(fmt, buff, 0)
+        broker_info, controller_id, topics = response
+        self._populate(broker_info, topics, controller_id=controller_id)
+
+
+class MetadataResponseV2(MetadataResponseV1):
+    """Response from MetadataRequest
+
+    Specification::
+
+    Metadata Response (Version: 2) => [brokers] cluster_id controller_id [topic_metadata]
+        brokers => node_id host port rack
+            node_id => INT32
+            host => STRING
+            port => INT32
+            rack => NULLABLE_STRING
+        cluster_id => NULLABLE_STRING  (new since v1)
+        controller_id => INT32
+        topic_metadata => error_code topic is_internal [partition_metadata]
+            error_code => INT16
+            topic => STRING
+            is_internal => BOOLEAN
+            partition_metadata => error_code partition leader [replicas] [isr]
+                error_code => INT16
+                partition => INT32
+                leader => INT32
+                replicas => INT32
+                isr => INT32
+    """
+    def __init__(self, buff):
+        fmt = '[iSiS] Si [hSb [hii [i] [i] ] ]'
+        response = struct_helpers.unpack_from(fmt, buff, 0)
+        broker_info, cluster_id, controller_id, topics = response
+        self._populate(broker_info, topics, controller_id=controller_id,
+                       cluster_id=cluster_id)
+
+
+class MetadataResponseV3(MetadataResponseV2):
+    """Response from MetadataRequest
+
+    Specification::
+
+    Metadata Response (Version: 3) => throttle_time_ms [brokers] cluster_id controller_id [topic_metadata]
+        throttle_time_ms => INT32  (new since v2)
+        brokers => node_id host port rack
+            node_id => INT32
+            host => STRING
+            port => INT32
+            rack => NULLABLE_STRING
+        cluster_id => NULLABLE_STRING
+        controller_id => INT32
+        topic_metadata => error_code topic is_internal [partition_metadata]
+            error_code => INT16
+            topic => STRING
+            is_internal => BOOLEAN
+            partition_metadata => error_code partition leader [replicas] [isr]
+                error_code => INT16
+                partition => INT32
+                leader => INT32
+                replicas => INT32
+                isr => INT32
+    """
+    def __init__(self, buff):
+        fmt = 'i [iSiS] Si [hSb [hii [i] [i] ] ]'
+        response = struct_helpers.unpack_from(fmt, buff, 0)
+        throttle_time_ms, broker_info, cluster_id, controller_id, topics = response
+        self._populate(broker_info, topics, controller_id=controller_id,
+                       cluster_id=cluster_id, throttle_time_ms=throttle_time_ms)
+
+
+class MetadataResponseV4(MetadataResponseV3):
+    """Response from MetadataRequest
+
+    Specification::
+
+    Metadata Response (Version: 4) => throttle_time_ms [brokers] cluster_id controller_id [topic_metadata]
+        throttle_time_ms => INT32
+        brokers => node_id host port rack
+            node_id => INT32
+            host => STRING
+            port => INT32
+            rack => NULLABLE_STRING
+        cluster_id => NULLABLE_STRING
+        controller_id => INT32
+        topic_metadata => error_code topic is_internal [partition_metadata]
+            error_code => INT16
+            topic => STRING
+            is_internal => BOOLEAN
+            partition_metadata => error_code partition leader [replicas] [isr]
+                error_code => INT16
+                partition => INT32
+                leader => INT32
+                replicas => INT32
+                isr => INT32
+    """
     pass
 
 
-class MetadataResponseV2(Response):
-    pass
+class MetadataResponseV5(MetadataResponseV4):
+    """Response from MetadataRequest
 
+    Specification::
 
-class MetadataResponseV3(Response):
-    pass
+    Metadata Response (Version: 5) => throttle_time_ms [brokers] cluster_id controller_id [topic_metadata]
+        throttle_time_ms => INT32
+        brokers => node_id host port rack
+            node_id => INT32
+            host => STRING
+            port => INT32
+            rack => NULLABLE_STRING
+        cluster_id => NULLABLE_STRING
+        controller_id => INT32
+        topic_metadata => error_code topic is_internal [partition_metadata]
+            error_code => INT16
+            topic => STRING
+            is_internal => BOOLEAN
+            partition_metadata => error_code partition leader [replicas] [isr] [offline_replicas]
+                error_code => INT16
+                partition => INT32
+                leader => INT32
+                replicas => INT32
+                isr => INT32
+                offline_replicas => INT32  (new since v4)
+    """
+    def __init__(self, buff):
+        fmt = 'i [iSiS] Si [hSb [hii [i] [i] [i]] ]'
+        response = struct_helpers.unpack_from(fmt, buff, 0)
+        throttle_time_ms, broker_info, cluster_id, controller_id, topics = response
+        self._populate(broker_info, topics, controller_id=controller_id,
+                       cluster_id=cluster_id, throttle_time_ms=throttle_time_ms)
 
-
-class MetadataResponseV4(Response):
-    pass
-
-
-class MetadataResponseV5(Response):
-    pass
+    def _build_partition_metas(self, partitions):
+        part_metas = {}
+        for (p_err, id_, leader, replicas, isr, offline_replicas) in partitions:
+            part_metas[id_] = PartitionMetadataV5(id_, leader, replicas,
+                                                  isr, offline_replicas, p_err)
+        return part_metas
 
 
 ##
