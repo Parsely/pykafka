d94bc46d238cd32b00dae2aead49310f455e1005
Emmett Butler <emmett.butler321@gmail.com>
2018-06-14 19:18:21 +0000

    increment dev version

--- pykafka/__init__.py
+++ pykafka/__init__.py
@@ -12,7 +12,7 @@ from .balancedconsumer import BalancedConsumer
 from .managedbalancedconsumer import ManagedBalancedConsumer
 from .membershipprotocol import RangeProtocol, RoundRobinProtocol
 
-__version__ = '2.8.0-dev.2'
+__version__ = '2.8.0-dev.3'
 
 
 __all__ = ["Broker", "SimpleConsumer", "Cluster", "Partition", "Producer",
