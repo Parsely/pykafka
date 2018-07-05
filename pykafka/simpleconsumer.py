2f5a96ed35d1ab5aa0ac02c0c881e34f8c0426e2
Emmett Butler <emmett.butler321@gmail.com>
2018-07-05 17:26:42 +0000

    call cleanup() in fetch instead of autocommitter
    wrap cleanup call in ReferenceError handler in case the fetcher thread died from a ReferenceError

--- pykafka/simpleconsumer.py
+++ pykafka/simpleconsumer.py
@@ -421,7 +421,6 @@ class SimpleConsumer(object):
                     # surface all exceptions to the main thread
                     self._worker_exception = sys.exc_info()
                     break
-            self.cleanup()
             log.debug("Autocommitter thread exiting")
         log.debug("Starting autocommitter thread")
         return self._cluster.handler.spawn(autocommitter, name="pykafka.SimpleConsumer.autocommiter")
@@ -444,6 +443,11 @@ class SimpleConsumer(object):
                     # surface all exceptions to the main thread
                     self._worker_exception = sys.exc_info()
                     break
+            try:
+                self.cleanup()
+            except ReferenceError as e:
+                log.debug("Attempt to cleanup consumer failed")
+                log.exception(e)
             log.debug("Fetcher thread exiting")
         log.info("Starting %s fetcher threads", self._num_consumer_fetchers)
         return [self._cluster.handler.spawn(fetcher, name="pykafka.SimpleConsumer.fetcher")
