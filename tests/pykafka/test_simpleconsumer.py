e8592b21482a2d5a0a03a47b88c18ac9fce709ba
Emmett Butler <emmett.butler321@gmail.com>
2018-07-23 22:56:03 +0000

    skipid -> skip for rdkafka simpleconsumer tst

--- tests/pykafka/test_simpleconsumer.py
+++ tests/pykafka/test_simpleconsumer.py
@@ -239,9 +239,10 @@ class TestSimpleConsumer(unittest2.TestCase):
             self.assertEqual(msg.offset, expected_offset)
             self.assertEqual(consumer.held_offsets[part_id], expected_offset)
 
-    @pytest.mark.skipif(RDKAFKA, reason="Unresolved crashes")
     def test_update_cluster(self):
         """Check that the consumer can initiate cluster updates"""
+        if self.RDKAFKA:
+            pytest.skip("Unresolved crashes")
         with self._get_simple_consumer() as consumer:
             self.assertIsNotNone(consumer.consume())
 
